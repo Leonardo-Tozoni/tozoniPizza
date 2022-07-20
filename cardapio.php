37ea9321bf7010df331935098203554bcc67e455
Leonardo Tozoni <leocebe2000@gmail.com>
2022-07-20 10:51:45 -0300

    cardapio changes

--- cardapio.php
+++ cardapio.php
@@ -1,25 +1,9 @@
 <?php
-include('config/config.php')
+include('config/config.php');
+$conexao = $conn->query ("SELECT * FROM valores "); 
+while($dados = $conexao->fetch_array()) { 
+    print_r($dados);
+}        
+
+
 ?>
-
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Retorno</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <td>Ingrediente</td>
-            <td>preço</td>
-    <?php 
-        $conexao = $conn->query ("SELECT * FROM valores "); 
-        while($dados = $conexao->fetch_array()) { ?>
-            <tr>
-            <td><?php echo $dados["ingrediente"];?></td>
-            <td>R$<?php echo $dados["valor"];?></td>
-            </tr>
-            
-        <?php }  ?>
-
-        </table>
